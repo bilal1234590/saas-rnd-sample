9d840831e1a5444ac22747b938200cbff23848e0
bilalg <girachbilal8@gmail.com>
2024-08-02 22:55:39 +0530

    reigstration and login page updating commit

--- src/auth/views.py
+++ src/auth/views.py
@@ -1,16 +1,33 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
 # Create your views here.
 def login_view(request):
-        username = "bilal_girach"#request.POST["username"]
-        password = "genshin88"#request.POST["password"]
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            print("login here!")
-            #return redirect("/")
+        if request.method == "POST":      
+            username = request.POST.get("username") or None
+            password = request.POST.get("password") or None
+            if all([username, password]):
+                user = authenticate(request, username=username, password=password)
+                if user is not None:
+                    login(request, user)
+                    print("login here!")
+                    return redirect("/")
         return render(request, "auth/login.html", {})
 
-#def register_view(request):
-    #return render(request, "auth/login.html", {})
+def register_view(request):
+    if request.method == "POST": 
+        print(request.POST)
+        username = request.POST.get("username") or None
+        email = request.POST.get("email") or None
+        password = request.POST.get("password") or None
+        #username_exists = User.objects.filter(username__iexcact=username).exists()
+        #email_exists = User.objects.filter(email__iexcact=email).exists()
+        try:          
+            User.objects.create_user(username, email=email, password=password)
+        except:
+             pass
+    return render(request, "auth/register.html", {})
